e63ef8652917b64d77c8de1909ad77ba2c7ecaab
arnold-graf <arnold.graf@gmail.com>
2019-08-26 17:18:03 +0200

    light theme

--- src/components/ScrollLockButton.vue
+++ src/components/ScrollLockButton.vue
@@ -2,9 +2,13 @@
   <v-btn
     fab
     small
-    :color="value ? 'grey darken-2' : undefined"
     @click="$emit('input', !value)"
-    class="lock-scroll-btn elevation-0">
+    :class="[
+      'lock-scroll-btn',
+      'elevation-0',
+      value && 'lock-active',
+      settings.darkMode === true && 'theme--dark'
+    ]">
     <div>
       <div class="bar" :style="{transform: `translate3d(${ value ? 0 : -3 }px, 0, 0)`}" />
       <div class="bar" :style="{top: '1px', transform: `translate3d(${ value ? 0 :  3 }px, 0, 0)`}" />
@@ -14,14 +18,18 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import settings from '../store/settings'
 
 @Component
 export default class ScrollLockButton extends Vue {
   @Prop({ default: false }) value: boolean
+  settings = settings
 }
 </script>
 <style lang="stylus" scoped>
-// .lock-scroll-btn
+.lock-scroll-btn.lock-active.theme--dark
+  background #616161 !important
+  border-color #616161 !important
 .bar
   position relative !important
   opacity .6 !important
@@ -29,5 +37,6 @@ export default class ScrollLockButton extends Vue {
   width 10px
   height 3px
   background white
-
+.theme--light .bar
+  background #111
 </style>
