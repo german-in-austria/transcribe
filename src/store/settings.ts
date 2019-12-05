427dd6cfd2066bc55b702e9959a309ad328d533b
arnold-graf <arnold.graf@gmail.com>
2019-12-05 14:39:18 +0100

    allow apostrophes (x') and interruptions at the beginning of the word (/xxx)

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -67,34 +67,28 @@ export const tokenTypesPresets: TokenTypesPreset = {
     {
       type: 'single',
       name: 'segments-unclear',
-      regex: /(\*)([a-zA-ZÜüÄäÖöß]+)(\*)/,
+      regex: /(\*)([a-zA-ZÜüÄäÖöß']+)(\*)/,
       color: '#6B6B6B',
       id: 9
     },
     {
       type: 'single',
       name: 'untransferable-lexics',
-      regex: /(_)([a-zA-ZÜüÄäÖöß]+)(_)/,
+      regex: /(_)([a-zA-ZÜüÄäÖöß']+)(_)/,
       color: '#FFAF3C',
       id: 8
     },
-    // {
-    //   name: 'quotation',
-    //   regex: /(„|")([a-zA-ZÜüÄäÖöß]+)(“|")/,
-    //   color: '',
-    //   id: -4
-    // },
     {
       type: 'single',
       name: 'interrupted',
-      regex: /([a-zA-ZÜüÄäÖöß]+\/$)/u,
+      regex: /([a-zA-ZÜüÄäÖöß']+\/$)|(\/[a-zA-ZÜüÄäÖöß']+$)/u,
       color: '#6699CC',
       id: 6
     },
     {
       type: 'single',
       name: 'pause',
-      regex: /\(\((([a-zA-ZÜüÄäÖöß]+)|(\d+(,\d)?s|))\)\)/u,
+      regex: /\(\((([a-zA-ZÜüÄäÖöß']+)|(\d+(,\d)?s|))\)\)/u,
       color: '#6B6B6B',
       id: 3
     },
@@ -102,7 +96,7 @@ export const tokenTypesPresets: TokenTypesPreset = {
       type: 'group',
       name: 'incomprehensible',
       bracketSymbols: [
-        /(\(([a-zA-ZÜüÄäÖöß\?]+))/u,
+        /(\(([a-zA-ZÜüÄäÖöß'\?]+))/u,
         /((.+)\))/u
       ],
       color: '#ccc',
@@ -112,7 +106,7 @@ export const tokenTypesPresets: TokenTypesPreset = {
       type: 'group',
       name: 'anonymized',
       bracketSymbols: [
-        /(\[([a-zA-ZÜüÄäÖöß\?]+))/u,
+        /(\[([a-zA-ZÜüÄäÖöß'\?]+))/u,
         /(.+\](N|O|Z|S))/,
       ],
       color: '#880000',
@@ -121,7 +115,7 @@ export const tokenTypesPresets: TokenTypesPreset = {
     {
       type: 'single',
       name: 'other',
-      regex: /\{([a-zA-ZÜüÄäÖöß]+)\}/u,
+      regex: /\{([a-zA-ZÜüÄäÖöß']+)\}/u,
       color: '#880000',
       id: 4
     },
@@ -135,7 +129,7 @@ export const tokenTypesPresets: TokenTypesPreset = {
     {
       type: 'single',
       name: 'word',
-      regex: /^([a-zA-ZÜüÄäÖöß"\-]+$)/u,
+      regex: /^([a-zA-ZÜüÄäÖöß'"\-]+$)/u,
       color: 'transparent',
       id: 1
     },
