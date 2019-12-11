f3173d742309a4498a6b373f3c834e3ad4a86d2e
arnold-graf <arnold.graf@gmail.com>
2019-12-11 20:36:49 +0100

    disabled checkboxes

--- src/components/helper/Checkbox.vue
+++ src/components/helper/Checkbox.vue
@@ -3,6 +3,7 @@
     <input
       class="mr-2"
       @change="$emit('input', !value)"
+      :disabled="disabled"
       :checked="value" type="checkbox" />
     <span style="user-select: none;" v-if="label !== undefined">{{ label }}</span>
     <slot v-else />
@@ -15,6 +16,7 @@ import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 export default class Checkbox extends Vue {
   @Prop() label?: string
   @Prop() value: boolean
+  @Prop({ default: false }) disabled: boolean
 }
 </script>
 <style lang="scss" scoped>
