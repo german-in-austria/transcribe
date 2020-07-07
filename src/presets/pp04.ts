35a0c1e91e9fb0c1b05bd5e173dcd9e11f225d95
arnold-graf <arnold.graf@gmail.com>
2020-07-07 19:04:02 +0200

    pp04 token types WIP

--- src/presets/pp04.ts
+++ src/presets/pp04.ts
@@ -4,23 +4,17 @@ import { ProjectPreset } from '.'
 export const PP04: ProjectPreset = {
   tokenizer: (s) => {
     return s
-      .split('.').join(' .')
-      .split(', ').join(' , ')
-      .split('-').join('_ _')
-      .split('? ').join(' ? ')
+      .split('_').join('_ _')
       .split(' ')
       .filter(t => t !== '')
   },
   tokenTypes: [
     {
-      type: 'group',
-      name: 'anonymized',
-      bracketSymbols: [
-        /(\[([a-zA-ZÜüÄäÖöß'\?]+))/u,
-        /(.+\](N|NZ|O|OP|OA|OS|P|Z))/,
-      ],
-      color: '#880000',
-      id: 10
+      type: 'single',
+      color: '#ccc',
+      id: 3,
+      regex: /\((((\.|-)+)|(\d+(.\d)?))\)/u,
+      name: 'pause'
     },
     {
       type: 'group',
@@ -33,6 +27,23 @@ export const PP04: ProjectPreset = {
       id: 7
     },
     {
+      type: 'single',
+      name: 'untransferable-lexics',
+      regex: /(_)([a-zA-ZÜüÄäÖöß']+)(_)/,
+      color: '#FFAF3C',
+      id: 8
+    },
+    {
+      type: 'group',
+      name: 'anonymized',
+      bracketSymbols: [
+        /(\[([a-zA-ZÜüÄäÖöß\.;,:\?-]+))/u,
+        /(.+\](N|NZ|O|OP|OA|OS|P|Z))/,
+      ],
+      color: '#880000',
+      id: 10
+    },
+    {
       type: 'group',
       name: 'non-verbal',
       bracketSymbols: [
@@ -41,6 +52,36 @@ export const PP04: ProjectPreset = {
       ],
       color: '#008800',
       id: 5
-    }
+    },
+    {
+      type: 'group',
+      name: 'other',
+      bracketSymbols: [
+        /\(\(.+[^\)]/u,
+        /[^\(\(].+\)\)/u
+      ],
+      color: 'gray',
+      id: 4
+    },
+    // TODO: show as error
+    {
+      type: 'group',
+      name: '',
+      bracketSymbols: [
+        /<<\S+>/u,
+        /^[^<<].+>/u
+      ],
+      color: 'orange',
+      id: 4 // other
+    },
+    // basic word tokens in GAT-2 can include
+    // markers for pitch, like ".", ":", etc.
+    {
+      type: 'single',
+      name: 'word',
+      regex: /^([a-zA-ZÜüÄäÖöß\.;,:\?-]+$)/u,
+      color: 'transparent',
+      id: 1
+    },
   ]
 }
