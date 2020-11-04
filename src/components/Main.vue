0adc8bb2de161ae0da959e6d7d9d909c468bbedf
arnold-graf <arnold.graf@gmail.com>
2020-11-04 16:24:52 +0100

    warning for non chromium browsers

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,23 +8,22 @@
 <script lang="ts">
 
 import { Vue, Component, Prop } from 'vue-property-decorator'
-// import 'material-icons'
-// import '@mdi/font'
-// import '../../node_modules/vuetify/dist/vuetify.min.css'
-// import '../../public/css/materialdesignicons.min.css'
-// import '../styles/fonts.css'
-// import '../styles/global.css'
 
 @Component
 export default class Main extends Vue {
+  mounted() {
+    const isChromium = (window as any).chrome !== undefined
+    if (!isChromium) {
+      alert('Transcribe currently only supports recent versions of Google Chrome, Chromium and Microsoft Edge browsers. Using other browsers might lead to unexpected or erroneous results.')
+    }
+  }
 }
 </script>
 
 <style lang="stylus">
-  // @import 'https://fonts.googleapis.com/icon?family=Material+Icons';
   @import '../../node_modules/@mdi/font/css/materialdesignicons.css'
   @import '../../node_modules/material-design-icons-iconfont/dist/material-design-icons.css'
-  @import '../../node_modules/vuetify/dist/vuetify.min.css';
+  @import '../../node_modules/vuetify/dist/vuetify.min.css'
   @import '../styles/fonts.css';
   @import '../styles/global.css';
 </style>
