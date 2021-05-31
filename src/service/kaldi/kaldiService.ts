a6747915f56e4538f04fc4e0891184b1c005986b
arnold-graf <arnold.graf@gmail.com>
2021-05-31 12:58:48 +0200

    kaldi: more progress events, reset after chunk transcript

--- src/service/kaldi/kaldiService.ts
+++ src/service/kaldi/kaldiService.ts
@@ -8,26 +8,26 @@ const models: {[url: string]: ArrayBuffer} = {}
 
 const asrHandler = new AsrHandler()
 const resampleHandler = new ResamplerHandler()
-type KaldiServiceStatus = 'DOWNLOADING_MODEL'|'INITIALIZING_MODEL'
+type KaldiServiceStatus = 'DOWNLOADING_MODEL'|'INITIALIZING_MODEL'|'PROCESSING_AUDIO'|'DONE'
 
 export default {
-  async transcribeAudio(modelUrl: string, buffer: AudioBuffer, cb: (status: KaldiServiceStatus) => any): Promise<string> {
+  async transcribeAudio(modelUrl: string, buffer: AudioBuffer, onUpdate: (status: KaldiServiceStatus, payload?: number) => any): Promise<string> {
     if (models[modelUrl] === undefined) {
-      console.log('downloading model…')
+      onUpdate('DOWNLOADING_MODEL')
       models[modelUrl] = await trackedDownload(modelUrl, console.log)
-      console.log('downloaded.')
-      console.log('initializing model…', modelUrl)
+      onUpdate('INITIALIZING_MODEL')
       await asrHandler.init('german', models[modelUrl])
-      console.log('initialized.')
     }
+    onUpdate('PROCESSING_AUDIO')
     const targetSampleRate = await asrHandler.getSampleRate()
     const sampleRate = buffer.sampleRate
     console.log({ targetSampleRate, sampleRate })
     // convert the PCM format from Float 32 to Int 16
     const audioData = sumChannels(buffer.getChannelData(0), buffer.getChannelData(1))
     const y = await resampleHandler.resample(audioData)
-    const x = await asrHandler.process(y)
-    await asrHandler.reset()
+    await asrHandler.process(y)
+    const x = await asrHandler.reset()
+    onUpdate('DONE')
     return x.text
     // asrHandler.
     // const resampledAudio = resampleAudio(audioData, 4096)
