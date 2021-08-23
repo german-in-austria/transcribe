e56e3c81fec954abb1a4f5f37c732238b2247fa9
arnold-graf <arnold.graf@gmail.com>
2021-08-23 12:42:13 +0200

    loading placeholder component

---
+++ src/components/WaveFormLoadingPlaceholder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-progress-circular indeterminate size="16" class="mr-3" width="2" />
+    Loading Audio …
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+@Component
+export default class WaveFormLoadingPlaceholder extends Vue {
+  mounted() {
+  }
+}
+</script>
+<style lang="stylus" scoped>
+div
+  height 300px
+  letter-spacing 0.1em
+  text-transform uppercase
+  align-items center
+  opacity 0.5
+  display flex
+  justify-content center
+</style>
