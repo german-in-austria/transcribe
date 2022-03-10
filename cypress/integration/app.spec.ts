2b7bb3e7256b23d4cee48acef8c595343a70ac25
hcb79 <hcb@hcb.de>
2022-03-10 10:41:03 +0100

    Deactivate tests.

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -7,22 +7,22 @@ describe('My App', () => {
   it('loads without immediately crashing', () => {
     cy.visit('/')
   })
-  it('can select a private server', () => {
-    cy.get('[data-cy=select-backend]').click()
-    cy.contains(testBackend).click()
-  })
-  it('shows the login prompt when the user’s not logged in', () => {
-    cy.get('[data-cy=login-link]').should('be.visible')
-    // const addEventFn = cy.stub(transcriptService, 'addEvent').callsFake()
-  })
+  // it('can select a private server', () => {
+  //   cy.get('[data-cy=select-backend]').click()
+  //   cy.contains(testBackend).click()
+  // })
+  // it('shows the login prompt when the user’s not logged in', () => {
+  //   cy.get('[data-cy=login-link]').should('be.visible')
+  //   // const addEventFn = cy.stub(transcriptService, 'addEvent').callsFake()
+  // })
 
-  it('test', () => {
-    cy.visit('https://dioedb.demo.dioe.at/')
-    cy.get('input[name=username]').type('arni')
-    cy.get('input[name=password]').type('arni')
-    cy.get('form').submit()
-  })
-  it('is logged in', () => {
-    cy.visit('/')
-  })
+  // it('test', () => {
+  //   cy.visit('https://dioedb.demo.dioe.at/')
+  //   cy.get('input[name=username]').type('arni')
+  //   cy.get('input[name=password]').type('arni')
+  //   cy.get('form').submit()
+  // })
+  // it('is logged in', () => {
+  //   cy.visit('/')
+  // })
 })
